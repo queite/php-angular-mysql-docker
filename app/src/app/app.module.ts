1abcdd331660bd982eb167d4c4b9c32da19973af
Queite <queitesc@gmail.com>
2023-08-14 18:56:41 -0300

    feat: :tada: start angular app

---
+++ app/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CourseComponent } from './course/course.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CourseComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
