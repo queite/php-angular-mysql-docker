693f6c2ae28514ff4a9c0a6271047ca7c02d0a51
Queite <queitesc@gmail.com>
2023-08-14 18:47:07 -0300

    feat: :card_file_box: implement connection

---
+++ api/php/connect.php
@@ -0,0 +1,19 @@
+<?php
+
+$servername = "mysql";
+$database = "api";
+$username = "root";
+$password = "root";
+
+// Connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+if (!$conn) {
+    exit('Connection failed: '.mysqli_connect_error().PHP_EOL);
+}
+
+echo 'Successful database connection!'.PHP_EOL;
+
+// Special characters
+mysqli_set_charset($conn, 'utf-8');
+
+?>
