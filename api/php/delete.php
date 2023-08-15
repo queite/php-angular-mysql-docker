b12c097c22b71017bc6a347fd66b3b53ce06a0a1
Queite <queitesc@gmail.com>
2023-08-15 12:39:34 -0300

    fix: :recycle: change the backend delete

--- api/php/delete.php
+++ api/php/delete.php
@@ -1,17 +1,22 @@
 <?php
 include('connect.php');
 
-//  Include connection
-$getData = file_get_contents('php://input');
-
-// Extract JSON data
-$extract = json_decode($getData);
-
-// Detach json data
-$id = $extract->courses->id;
+// Extract query parameters
+$id = $_GET['id'];
 
 // SQL
-$sql = 'DELETE FROM courses  WHERE id=$id';
-mysqli_query($conn, $sql);
+$sql = "DELETE FROM courses WHERE id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param('i', $id);
+$result = $stmt->execute();
 
+if ($result) {
+    $response = array("message" => "Course deleted successfully");
+    http_response_code(200);
+} else {
+    $response = array("message" => "Error deleting course");
+    http_response_code(500);
+}
+
+echo json_encode($response);
 ?>
