1ec82c7e54c6ee09c246d39c24fd134fa1ba4387
Queite <queitesc@gmail.com>
2023-08-15 16:42:31 -0300

    refactor: :sparkles: refactor update on backend

--- api/php/update.php
+++ api/php/update.php
@@ -1,27 +1,27 @@
 <?php
 include('connect.php');
 
-//  Include connection
+// Include connection
 $getData = file_get_contents('php://input');
 
 // Extract JSON data
 $extract = json_decode($getData);
 
 // Detach json data
-$id = $extract->courses->id;
-$courseName = $extract->courses->courseName;
-$coursePrice = $extract->courses->coursePrice;
+$id = $extract->id;
+$courseName = $extract->courseName;
+$coursePrice = $extract->coursePrice;
 
 // SQL
-$sql = 'UPDATE courses SET course_name="$courseName", course_price=$coursePrice WHERE id=$id';
+$sql = "UPDATE courses SET course_name='$courseName', course_price=$coursePrice WHERE id=$id";
 mysqli_query($conn, $sql);
 
-//Export data
-$course = [
-  'id' = $id;
-  'courseName' = $courseName;
-  'coursePrice' = $coursePrice;
-]
+// Export data
+$course = array(
+  'id' => $id,
+  'courseName' => $courseName,
+  'coursePrice' => $coursePrice
+);
 
-json_encode(['course'] => $course)
+echo json_encode(array('course' => $course));
 ?>
